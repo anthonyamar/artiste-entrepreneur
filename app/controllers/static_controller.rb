dbc6643bee35d4d8d581895cd0c36896fe97c0d1
anthonyamar <anthonyamar@labodessons.fr>
2020-11-23 19:10:17 +0100

    add full program to landing

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   
   def home
+    @courses = I18n.t("courses")
   end
   
   def legal
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module AeRailsWw
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # I18n config
+    config.i18n.available_locales = [:fr, :en]
+    config.i18n.default_locale = :fr
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.fallbacks = [I18n.default_locale]
+    
   end
 end
